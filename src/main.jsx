b97feb30f53e5a0d536284654008d43f33e4ea3e
Baba Faye <fayebaba81@gmail.com>
2025-05-05 11:41:21 +0000

    modification2

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./Components/formulaires/AuthContext.jsx";
+import { FormProvider } from "./Components/formulaires/FormContext.jsx";
 import { ReservationProvider } from "./components/ReservationContext.jsx";
-import { FormProvider } from "./components/Formulaires/FormContext.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
